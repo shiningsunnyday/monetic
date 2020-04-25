c94b658d1b49e249d860db009608acb6812b6bca
Michael Sun <michaelsun18@yahoo.com>
2020-04-25 08:00:35 -0700

    add route to donation, route on click and gofundme jsx

---
+++ monetic/src/pages/gofundme.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class GoFundMe extends React.Component{
+  render(){
+    return(
+      <embed height="500px" width="100%" src="https://www.gofundme.com/mvc.php?route=widgets/mediawidget&fund=monetic&image=1&coinfo=1&preview=1" type="text/html"></embed>
+    );
+  }
+}
