aaaa78164624a784a9d43aa2175ca5c1ea8f905d
Kevin <kevin.ko@berkeley.edu>
2020-04-25 14:58:12 -0700

    reverting

--- monetic/src/pages/gofundme.jsx
+++ monetic/src/pages/gofundme.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class GoFundMe extends React.Component{
   render(){
     return(
-      <embed width="50%" src="https://www.gofundme.com/mvc.php?route=widgets/mediawidget&fund=monetic&image=1&coinfo=1&preview=1" type="text/html"></embed>
+      <embed height="500px" width="100%" src="https://www.gofundme.com/mvc.php?route=widgets/mediawidget&fund=monetic&image=1&coinfo=1&preview=1" type="text/html"></embed>
     );
   }
 }
