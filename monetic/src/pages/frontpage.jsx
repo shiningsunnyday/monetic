d4be3a4e4b526c041d5755f6ad6db721c381b54a
Kevin <kevin.ko@berkeley.edu>
2020-04-25 23:43:49 -0700

    next button works. made repeating videos on refresh less likely.

--- monetic/src/pages/frontpage.jsx
+++ monetic/src/pages/frontpage.jsx
@@ -1,26 +1,62 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/frontpage.css'
 import {
   Link,
 } from "react-router-dom";
 import TikTok from './tiktok'
-import Form from './form'
 import Header from './header';
-import Dropdown from 'react-bootstrap'
+import firebase from '../firebase.js';
 
 // import Card from './card';
 
 export default class FrontPage extends React.Component{
   state = {
-    index: 0,
+    ind: 0,
   }
 
   constructor(props) {
     super(props);
-  
+    this.nextVideo = this.nextVideo.bind(this);
+      
+  }
+
+  nextVideo() {
+    console.log("refreshing");
+    this.componentDidMount();
+    
+  }
+
+  componentDidMount() {
+    const ref = firebase.database().ref();
+    ref.on("value", (snapshot) => {
+      let val = snapshot.val();
+      let numKeys = Object.keys(val).length
+      
+      console.log("last ind: " + this.state.ind);
+      let index = Math.floor(Math.random() * numKeys);
+      if (index === this.state.ind) {
+        
+        index = Math.floor(Math.random() * numKeys);
+        console.log("collison. new index: " + index);
+      }
+
+      let key = Object.keys(val)[index];
+      const childRef = ref.child(key);
+      childRef.on("value", (snapshot) => {
+        let val = snapshot.val();
+        let link = 'https://www.tiktok.com/@' + key + '/video/' + Object.keys(val)[0];
+        fetch('https://www.tiktok.com/oembed?url=' + link)
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ 
+            tiktoks: data,
+            ind: index,   
+          })
+        })
+        .catch(console.log);
+      })});
   }
   
   render() {
@@ -38,10 +74,10 @@ export default class FrontPage extends React.Component{
             <Button variant="success">Donate</Button>
           </Link>{' '}
           
-          <Button variant="warning">Next</Button>{' '}
+          <Button variant="warning" onClick={this.nextVideo}>Next</Button>{' '}
         </div>
 
-        <TikTok updateFund={(ind)=>this.setState({index: ind})}></TikTok>
+        <TikTok tiktoks={this.state.tiktoks}></TikTok>
       </div>
 
     );
--- monetic/src/pages/tiktok.jsx
+++ monetic/src/pages/tiktok.jsx
@@ -3,46 +3,25 @@ import React from 'react';
 import firebase from '../firebase.js';
 
 export default class TikTok extends React.Component {
-  state = {
-    tiktoks: []
+  constructor(props) {
+    super(props);
   }
 
-  componentDidMount() {
-
-    const ref = firebase.database().ref();
-    ref.on("value", (snapshot) => {
-      let val = snapshot.val();
-      let numKeys = Object.keys(val).length
-      let index = Math.floor(Math.random() * numKeys);
-      let key = Object.keys(val)[index];
-      const childRef = ref.child(key);
-      childRef.on("value", (snapshot) => {
-        let val = snapshot.val();
-        let link = 'https://www.tiktok.com/@' + key + '/video/' + Object.keys(val)[0];
-        fetch('https://www.tiktok.com/oembed?url=' + link)
-        .then(res => res.json())
-        .then((data) => {
-          this.setState({ tiktoks: data });
-          this.props.updateFund(index);
-        })
-        .catch(console.log);
-
-      })});
+  componentWillReceiveProps() {
+    this.setState({})
   }
-
   render() {
     var script = document.createElement("script");
     script.src = "https://www.tiktok.com/embed.js";
     document.head.appendChild(script)
-
-
+  
     return(
       <div dangerouslySetInnerHTML={this.createHTML()} />
     );
   }
 
   createHTML() {
-    return {__html: this.state.tiktoks?.html};
+    return {__html: this.props.tiktoks?.html};
   }
 
 }
