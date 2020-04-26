2118ce835eb7adc36b0b294ea7139491d0691e41
Kevin <kevin.ko@berkeley.edu>
2020-04-25 20:50:12 -0700

    signup page

--- monetic/src/App.js
+++ monetic/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import FrontPage from './pages/frontpage.jsx';
 import Donate from './pages/donate.jsx';
+import Signup from './pages/signup.jsx';
 import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Switch>
           <Route exact path="/" component={FrontPage}/>
           <Route exact path="/donate" component={Donate}/>
+          <Route exact path="/signup" component={Signup}/>
           <Redirect to="/404"/>
         </Switch>
     </Router>
