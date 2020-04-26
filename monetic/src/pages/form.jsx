dbb86f81f4b2c1899da75019bfb7ceb27e077563
Kevin <kevin.ko@berkeley.edu>
2020-04-25 21:02:26 -0700

    push

--- monetic/src/pages/form.jsx
+++ monetic/src/pages/form.jsx
@@ -7,7 +7,13 @@ export default class Form extends React.Component {
     this.state = {
       tiktok: '',
       gofundme: '',
-    
+      address: '',
+      city: '',
+      state: '',
+      zipcode: '',
+      occupation: '',
+      householdsize: '',
+      incomeloss: '',   
     };
 
       this.handleChange = this.myChangeHandler.bind(this);
@@ -21,9 +27,9 @@ export default class Form extends React.Component {
     }
   
     handleSubmit(event) {
-      console.log('tiktok: ' + this.state.tiktok);
-      console.log('gofundme: ' + this.state.gofundme);
-      event.preventDefault(); 
+      console.log('state: ' + this.state.tiktok);
+      console.log('state: ' + this.state.address);
+      // event.preventDefault(); 
     }
 
     
@@ -39,6 +45,41 @@ export default class Form extends React.Component {
           <p>
             GofundMe Id:
             <input type="text" name='gofundme' value={this.state.gofundme} onChange={this.myChangeHandler} />
+          </p>
+
+          <p>
+            Address:
+            <input type="text" name='address' value={this.state.address} onChange={this.myChangeHandler} />
+          </p>
+
+          <p>
+            City:
+            <input type="text" name='city' value={this.state.city} onChange={this.myChangeHandler} />
+          </p>
+
+          <p>
+            State:
+            <input type="text" name='state' value={this.state.state} onChange={this.myChangeHandler} />
+          </p>
+
+          <p>
+            Zip Code:
+            <input type="text" name='zipcode' value={this.state.zipcode} onChange={this.myChangeHandler} />
+          </p>
+
+          <p>
+            Occupation:
+            <input type="text" name='occupation' value={this.state.occupation} onChange={this.myChangeHandler} />
+          </p>
+
+          <p>
+            Household Size:
+            <input type="text" name='householdsize' value={this.state.householdsize} onChange={this.myChangeHandler} />
+          </p>
+
+          <p>
+            Income loss due to Covid-19:
+            <input type="text" name='incomeloss' value={this.state.incomeloss} onChange={this.myChangeHandler} />
           </p>
 
           <Button variant="success" value="Submit" type="submit">Submit</Button>{' '}
